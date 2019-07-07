9abe68d039236a9ff09d6f54e3f8434f8293f9f3
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2019-07-06 21:56:23 -0700

    Created dummy classes to load with main

--- main.go
+++ main.go
@@ -1,21 +1,42 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request){
-  fmt.Fprintf(w, "Welcome to the Homepage!")
-  fmt.Println("Endpoint hit: homePage")
+// Class - Struct for D&D Classes
+type Class struct {
+	Name string `json:"name"`
 }
 
-func handleRequests(){
-  http.HandleFunc("/", homePage)
-  log.Fatal(http.ListenAndServe(":10000", nil))
+var Classes []Class
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Homepage!")
+	fmt.Println("Endpoint hit: homePage")
 }
 
-func main(){
-  handleRequests()
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/classes", returnAllClasses)
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func returnAllClasses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Show all classes")
+	json.NewEncoder(w).Encode(Classes)
+}
+
+func main() {
+	Classes = []Class{
+		Class{Name: "Cassy"},
+		Class{Name: "Cindy"},
+	}
+	handleRequests()
 }
